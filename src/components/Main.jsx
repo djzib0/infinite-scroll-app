9128abecae8e6636248f3f8f877264747f105d6c
djzib0 <piotrsabatura@gmail.com>
2023-12-14 18:00:45 +0100

    added styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,7 +19,7 @@ function Main() {
   const citiesArr = citiesData && citiesData.slice(0, sliceSize).map(city => {
       return <div 
       key={nanoid()}
-      className='data-row'
+      className='data-container'
       >{city.name.common}</div>
   })
 
@@ -27,7 +27,7 @@ function Main() {
       if (citiesData) {
         setTimeout(() => {
           if (sliceSize + 10 < citiesData.length) {
-            setSliceSize(prevState => prevState + 10)
+            setSliceSize(prevState => prevState + 9)
           } else {
             // sliceSize cannot be greater than length of data array.
             setSliceSize(prevState => citiesData.length)
@@ -44,37 +44,37 @@ function Main() {
     setHasMore(true)
   }
 
+  // adds new records to the data array to show the infinite scroll is working again after
+  // adding a new data
   function addNewRecords() {
     const records = newRecords;
     setCitiesData(prevState => [...prevState, ...records])
     setHasMore(true)
     if (sliceSize + 10 < citiesData.length) {
-      setSliceSize(prevState => prevState + 10)
+      setSliceSize(prevState => prevState + 9)
     }
   }
-
-  console.log(hasMore + " <= hasMore")
 
   return (
     <div>
       <button className='fixed-btn' onClick={() => resetInfiniteScroll()}>GO TOP</button>
       <button className='fixed-btn-add' onClick={() => addNewRecords()}>Add 10 new records</button>
-    <InfiniteScroll
-      datalength={citiesArr && citiesArr.slice(0, sliceSize).length} 
-      next={fetchMoreData} 
-      hasMore={hasMore}
-      loader={<h4 key={nanoid()}>Loading...</h4>}
-      loadMore={fetchMoreData}
-      endMessage={
-        <p style={{ textAlign: 'center' }}>
-          <b>There is no more data!</b>
-        </p> // it doesn't work :( find solution
-      }
-      >
-      <div>{citiesArr}</div>
-    </InfiniteScroll>
-    {/* my solution to override not working endMessage in InfiniteScroll component  */}
-    {!hasMore && <div><b>There is no more data!</b></div>} 
+      <InfiniteScroll
+        datalength={citiesArr && citiesArr.slice(0, sliceSize).length} 
+        next={fetchMoreData} 
+        hasMore={hasMore}
+        loader={<h4 key={nanoid()}>Loading...</h4>}
+        loadMore={fetchMoreData}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>There is no more data!</b>
+          </p> // it doesn't work :( find solution
+        }
+        >
+          <div className='container'>{citiesArr}</div>
+        </InfiniteScroll>
+      {/* my solution to override not working endMessage in InfiniteScroll component  */}
+      {!hasMore && <div><b>There is no more data!</b></div>} 
     </div>
   )
 }
